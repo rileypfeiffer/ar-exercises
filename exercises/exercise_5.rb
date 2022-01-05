7d9bf7366e05cc34ad52ec9d62137585da219410
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2022-01-05 15:59:03 -0500

    Complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts @total_revenue
+@average_revenue = Store.average(:annual_revenue)
+puts @average_revenue
+@over_a_million = Store.where("annual_revenue > ?", 1_000_000).count
+puts @over_a_million
