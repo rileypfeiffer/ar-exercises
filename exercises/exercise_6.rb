2a506d9c6368e8c80ac8f29e6f2c0a27942bd4f8
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2022-01-05 16:19:29 -0500

    Complete exercises 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Riley", last_name: "Pfeiffer", hourly_rate: 40)
+@store2.employees.create(first_name: "John", last_name: "Jeffries", hourly_rate: 50)
+@store2.employees.create(first_name: "Jimmy", last_name: "John", hourly_rate: 30)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,12 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Enter a store name"
+input = gets.chomp
+
+new_store = Store.new
+new_store_name = input
+new_store.save
+
+puts new_store.errors.full_messages
